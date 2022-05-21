8bf9a6c0c941b2f81cd79dec009a47da0c03c6f5
Átila Cassel <atilacassel@C02DC3L3MD6P.local>
2022-05-21 15:14:31 -0300

    Creation BaseTest and Constants

--- src/test/java/core/Constants.java
+++ src/test/java/core/Constants.java
@@ -1,5 +1,12 @@
 package core;
 
+import io.restassured.http.ContentType;
+
 public interface Constants {
-    String BASE_URL =
+
+    String APP_BASE_URL = "http://localhost:3000/";
+
+    ContentType APP_CONTENT_TYPE = ContentType.JSON;
+
+    Long MAX_TIMEOUT = 1000l;
 }
